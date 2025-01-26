c92d81d959692de701dcd22ea33c5f03c65d24bf
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2025-01-26 12:56:58 +0000

    Remove git configurations in post-create-script

--- .scripts/post-create-script.bash
+++ .scripts/post-create-script.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-#git config --global user.email "rishikesavanramesh@proton.me"
-#git config --global user.name "Rishikesavan Ramesh"
-
 #bash /workspace/.scripts/print_packages.bash
 
