e9c8efd3fe18666e0e3a530be5bd61027b222b3f
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2025-01-26 11:47:25 +0000

    Remove package printing script
    Signed-off-by: Rishikesavan Ramesh <rishikesavanramesh@proton.me>

--- .scripts/post-create-script.bash
+++ .scripts/post-create-script.bash
@@ -3,5 +3,5 @@
 # git config --global user.email "rishikesavanramesh@proton.me"
 # git config --global user.name "Rishikesavan Ramesh"
 
-bash /workspace/.scripts/print_packages.bash
+#bash /workspace/.scripts/print_packages.bash
 
