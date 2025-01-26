5927360286c8f46a5059e1f928be64313b4a556d
Rishikesavan Ramesh <rishikesavanramesh@proton.me>
2025-01-26 01:32:55 +0000

    disable git config in post-create script

--- .scripts/post-create-script.bash
+++ .scripts/post-create-script.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.email "rishikesavanramesh@proton.me"
-git config --global user.name "Rishikesavan Ramesh"
+# git config --global user.email "rishikesavanramesh@proton.me"
+# git config --global user.name "Rishikesavan Ramesh"
 
 bash /workspace/.scripts/print_packages.bash
 
